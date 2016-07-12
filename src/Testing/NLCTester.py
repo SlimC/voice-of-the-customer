06b2ac7e10306263f58a35e42a4613592f42e53a
MATTHEW E. WASSEL <mewassel@us.ibm.com>
2016-07-12 11:41:17 -0500

    Refactored scripts to use cloudant package rather than couchdbkit

--- src/Testing/NLCTester.py
+++ src/Testing/NLCTester.py
@@ -1,7 +1,7 @@
 
 import csv
 import nltk
-import couchdbkit
+import cloudant
 import json
 from watson_developer_cloud import NaturalLanguageClassifierV1
 
@@ -10,7 +10,8 @@ SERVER = ''													#Replace with your server URL
 DATABASE = ''												#Replace with the name of the database
 USERNAME = ''												#Replace with the username from your credentials for the NLC
 PASSWORD = ''												#Replace with the password from your credentials for the NLC
-VIEW = 		''												#Replace with the view from your database to poll, this should take the form of /view_file/view and should return the text to classify
+DESIGN = ''													#replace with the name of the design document that contains the view. This should be of the form '_design/XXXX'
+VIEW = 		''												#Replace with the view from your database to poll, be the name of the view and should return the text to classify
 RESULTFILE = ''												#Replace with where to store the result file in csv format
 CLASSIFIERID = ''											#Replace with the id of the classifier to use to classify
 
@@ -48,22 +49,22 @@ def classify_sentence(classifier, token):
 		results.append(confidence)
 	return results
 
-server = couchdbkit.Server(SERVER)
-db = server.get_or_create_db(DATABASE)
-query = db.view(VIEW)
+server = cloudant.client.Cloudant(USERNAME,PASSWORD,url=SERVER)
+server().connect
+db = server[DATABASE]
+
+result = db.get_view_result(DESIGN,VIEW)
 
 nlc = NaturalLanguageClassifierV1(username = USERNAME, password = PASSWORD)
 
-
-data = json.loads(json.dumps(query.all()))
 
 clean = open(RESULTFILE, 'w')
 writer = csv.writer(clean)
 
 
-for i in range(0, len(data)):
-	id = data[i].get('id')
-	sentences = nltk.tokenize.sent_tokenize(data[i].get('value'))
+for result in view:
+	id = result[0].get('id')
+	sentences = nltk.tokenize.sent_tokenize(result[0].get('value'))
 	for j in sentences:
 		if len(j) > 1024:
 			split_long_string(j, id, nlc)  #IMPORTANT: Comment out this line and uncomment the next if you do not want to split strings that are too long. (Useful if comparing index to index)
