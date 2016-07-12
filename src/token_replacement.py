041b61ac01423e60bd594eed4716f0696467ff6d
SUKRITI SHARMA <sukritis@us.ibm.com>
2016-07-12 11:54:57 -0500

    changes to dict

--- src/token_replacement.py
+++ src/token_replacement.py
@@ -10,27 +10,42 @@ def get_relations(review):
 	response = ast.literal_eval(response)
 	return response
 
-def get_entities(review)
-	url = "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities?showSourceText=1&model=8f03f142-af7f-4487-be47-371fc3262705&apikey=ffd7397f4be657f7740a84038f903271b2707a11&outputMode=json&sentiment=1"
+def get_entities(review):
+	url = "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities?showSourceText=1&model=ae997404-c8d5-433a-995c-dceeacf22e34&apikey=ffd7397f4be657f7740a84038f903271b2707a11&outputMode=json&sentiment=1"
 	f = requests.get(url, params={'text':review})
 	response = f.content
 	response = ast.literal_eval(response)
 	return response
 
-def token_replacement_entities(reivew):
+def token_replacement_entities(review):
+	review_dict={}
 	review = get_entities(review)
+	pp.pprint(review)
 	if 'entities' in review:
 		entities = review['entities']
 		text = review['text']
 		for i in entities:
+			temp={}
 			token = i['text']
-			count = i['count']
+			count = int(i['count'])
+			#temp[]=i['text']
+			temp['count']=1
+                        temp['sentiment']=[i['sentiment']['type']]
+			print temp
+			#if i['type'] in review_dict:
+				
+			review_dict[i['type']]={}
+			review_dict[i['type']][i['text']]=temp
+			
+			#review_dict[i['type']].append(temp)			
 			classification = "<" + i['type'] + ">"
 			text = text.replace(token,classification,count)
+	print review_dict
 	return text
 
 def token_replacement_relations(review):
 	review = get_relations(review)
+	#pp.pprint(review)	
 	if 'typedRelations' in review:
 		types = review['typedRelations']
 		text = review['text']
@@ -41,6 +56,7 @@ def token_replacement_relations(review):
 		flip = True
 		sentence_dict={}
 		prev_dict={}
+		result=[]
 		if types != []:
 			count=0
 			result=[]
@@ -77,15 +93,15 @@ def token_replacement_relations(review):
 								dict[i['type']].append(temp_dict)
 							else:
 								dict[i['type']]=[]
-                                dict[i['type']].append(temp_dict)
+                                				dict[i['type']].append(temp_dict)
 						else:
 							sec_dict={}
 							sec_dict['name']=i['text']
 							if i['type'] in dict:
-                                dict[i['type']].append(sec_dict)
-                            else:
+                                				dict[i['type']].append(sec_dict)
+                            				else:
 								dict[i['type']]=[]
-                                dict[i['type']].append(sec_dict)
+                                				dict[i['type']].append(sec_dict)
 						classification = "<" + i['type'] + ">"
 						if flip:
 							review_text_mod = review_text.replace(token,classification,1)
@@ -95,19 +111,19 @@ def token_replacement_relations(review):
 							flip = True
 				dict['reversed_sentence']=review_text_mod
 				#print temp_dict
-				print "final dict"
-				print dict
+				#print "final dict"
+				#print dict
 				prev_dict=dict
 				result[sentence_dict[sentence['sentence']]]=dict
 				#result.append(dict)
-				print "new"
+				#print "new"
 				#print sentence_dict
-		pp.pprint(result)
+		#pp.pprint(result)
 		text = text.replace(standard,review_text_mod,1)
-		return text
+		return result
 		#pp.print(result
 	else: 
-		return review
+		return result
 
-
-print token_replacement('This TV has good picture quality and this radio has good sound. I bought it for 500 dollars');
+#print token_replacement_entities('This TV has good picture quality and this radio has good sound. I bought it for 500 dollars. . I like this TV. I do not like the radio.');
+#print token_replacement_relations('This TV has good picture quality and this radio has good sound. I bought it for 500 dollars. I like this TV. I do not like the radio.');
