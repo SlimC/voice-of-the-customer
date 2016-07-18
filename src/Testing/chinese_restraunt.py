98d53b2c37b813df80007eb0d06641e35fb369ca
MATTHEW E. WASSEL <mewassel@us.ibm.com>
2016-07-18 14:50:04 -0500

    Added the chinese restraunt script, and the script that creates a custom word2vec model

--- src/Testing/chinese_restraunt.py
+++ src/Testing/chinese_restraunt.py
@@ -2,43 +2,60 @@ import numpy as np
 import scipy.spatial.distance as distance
 
 def crp(vecs):
-    clusterVec = [0]         # tracks sum of vectors in a cluster
-    clusterIdx = [0]         # array of index arrays. e.g. [[1, 3, 5], [2, 4, 6]]
-    ncluster = 0
-    # probablity to create a new table if new customer
-    # is not strongly "similar" to any existing table
+    clusterVec = [vecs[0]]         # tracks sum of vectors in a cluster
+    clusterIdx = [[0]]         # array of index arrays. e.g. [[1, 3, 5], [2, 4, 6]]
+    ncluster = 1
+    print clusterVec
+    # # probablity to create a new table if new customer
+    # # is not strongly "similar" to any existing table
     pnew = 1.0/ (1 + ncluster)
     N = len(vecs)
-    rands = np.random.rand(N)         # N rand variables sampled from U(0, 1)
-
-    for i in range(N):
-        maxSim = - np.inf
-        maxIdx = 0
+    rand = np.random.rand         #rand variables sampled from U(0, 1)
+    for i in range(1,N):
+        print i
+        maxSim = -np.inf
+        maxIdx = None
         v = vecs[i]
         for j in range(ncluster):
+            print v.shape
+            print clusterVec[j].shape
             sim = distance.cosine(v, clusterVec[j])
-            if sim < maxSim:
-                maxIdx = j
-                maxSim = sim
-            if maxSim < pnew:
-                if np.numpy.random.rand(i) < pnew:
-                    clusterVec[ncluster] = v
-                    clusterIdx[ncluster] = [i]
+            if sim > pnew:
+                if sim > maxSim:
+                    maxIdx = j
+                    sim = maxSim
+                    print "maxId: " + str(j)
+        print maxIdx
+        if maxIdx is not None:
+            clusterVec[maxIdx] += v
+            clusterIdx[maxIdx].append(i)
+            print "appended: ids = " + str(clusterIdx)
+        else:
+            for j in range(ncluster):
+                if rand() < pnew:
+                    clusterVec.append(v)
+                    clusterIdx.append([i])
                     ncluster += 1
-                    pnew = 1.0 / (1 + ncluster)
-                continue
-        clusterVec[maxIdx] = clusterVec[maxIdx] + v
-        clusterIdx[maxIdx].append(i)
-
+                    pnew = 1.0/(1+ncluster)
+                    print clusterVec
+                    print clusterIdx
+                    print ncluster
+                    print pnew
+                    break
+                else:
+                    clusterVec[j] += v
+                    clusterIdx[j].append(i)
+                    print clusterVec
+                    print clusterIdx
+                    break
     return clusterIdx
 
 def word_cluster():
     keywords = np.load('keywords.npy')
     vectors = np.load('keywords_vecs.npy')
-
     cluster_vecs = crp(vectors)
-
-    for cluster in cluster_vecs:
-        for word in cluster:
-            cluster_vecs[cluster][word] = keywords[word]
-    return clusters
+    print type(cluster_vecs)
+    for i in range(len(cluster_vecs)):
+        for j in range(len(cluster_vecs[i])):
+            cluster_vecs[i][j] = keywords[cluster_vecs[i][j]]
+    return cluster_vecs
