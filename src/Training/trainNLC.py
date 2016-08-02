2d30c0f7f5cf669ad8c10e46f6b1bfd8bef8a6f2
SUKRITI SHARMA <sukritis@us.ibm.com>
2016-08-02 09:41:50 -0500

    test sets

--- src/Training/trainNLC.py
+++ src/Training/trainNLC.py
@@ -3,11 +3,11 @@ import json
 from watson_developer_cloud import NaturalLanguageClassifierV1
 import csv
 
-tier1CSV = 'training_set1.csv'
-tier2CSV = 'training_set2.csv'
-tier3CSV = 'training_set3.csv'
-USERNAME = 'e561bc30-d294-41f4-8b47-39fc6bc29917'
-PASSWORD = 'XH8pYnsYfClv'
+tier1CSV = 'new train sets/training_layer1_replace_no_descriptor.csv'
+tier2CSV = 'new train sets/training_layer2_replace_no_descriptor.csv'
+#tier3CSV = 'training_set3.csv'
+USERNAME = 'a6f087f2-73d6-4d33-bece-821ec3c5b96a'
+PASSWORD = '8StZ2fxq0eFF'
 JSON_TARGET = '../../data/classifier_ids.json'
 
 classifierTree = {
@@ -41,6 +41,7 @@ with open(tier2CSV, 'rb') as training_data:
 print(json.dumps(classifier, indent=2))
 classifierTree['tier2'] = classifier['classifier_id']
 
+'''
 # Train tier 3 classifier
 print("############# TIER 3 CLASSIFIER ##############")
 with open(tier3CSV, 'rb') as training_data:
@@ -51,6 +52,7 @@ with open(tier3CSV, 'rb') as training_data:
   )
 print(json.dumps(classifier, indent=2))
 classifierTree['tier3'] = classifier['classifier_id']
+'''
 
 # Write the tiers with classifier id's to file for use later
 with open(JSON_TARGET, 'w') as outfile:
